1351ec5f69afce9e2da69447e0fc0b363a358ef8
Quintin Dunn <93884113+quintindunn@users.noreply.github.com>
2023-12-19 01:48:20 -0500

    Setup HOTFile structure

--- repacker.py
+++ repacker.py
@@ -3,9 +3,7 @@ from io import BytesIO
 import os
 import struct
 
-from head_sizes import get_head_size
-
-file_name = input("File name: ") or "textures_new.hot"
+file_name = input("File name: ") or "out.hot"
 files_dir = input("Files Dir: ") or "./OUTPUT/textures"
 
 files = os.listdir(files_dir)
@@ -16,5 +14,24 @@ class HOTFile:
         self.file_names = os.listdir(input_dir)
         self.files_root = input_dir
 
-        self.header = "HOT ".encode("utf-8")
+        self.content = []
 
+        self._write("HOT ".encode('utf-8'))
+
+    def _write(self, content, idx: int | None = None):
+        if idx is not None:
+            return self.content.insert(idx, content)
+        return self.content.append(content)
+
+    def to_io(self):
+        io = BytesIO()
+        io.write(b''.join(self.content))
+        return io
+
+
+if __name__ == '__main__':
+    hot = HOTFile(input_dir=files_dir)
+    hot._write(struct.pack("<IIIIIIII", 1, 2, 3, 4, 5, 6, 7, 8))
+
+    with open(file_name, 'wb') as f:
+        f.write(hot.to_io().getvalue())
